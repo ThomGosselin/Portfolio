2160ef9dff05cbcde8a768acf98bf866fef3a5c4
ThomGosselin <thomswim360@gmail.com>
2022-05-19 14:56:38 -0400

    Start adding video player in video page

--- js/main.js
+++ js/main.js
@@ -20,14 +20,3 @@ var app = function () {
 }
 ();
 
-let buttonCV = document.querySelector(".buttonCV");
-let CV = document.querySelector(".CV");
-let close = document.querySelector(".close");
-buttonCV.onclick = function(){
-    buttonCV.classList.add('active');
-    CV.classList.add('active');
-}
-close.onclick = function(){
-    buttonCV.classList.remove('active');
-    CV.classList.remove('active');
-}
---
+++ js/video.js
@@ -0,0 +1,11 @@
+let buttonVideo = document.querySelector(".animatedButton");
+let videoProject = document.querySelector(".videoProject");
+let close = document.querySelector(".close");
+buttonVideo.onclick = function(){
+    buttonVideo.classList.add('active');
+    videoProject.classList.add('active');
+}
+close.onclick = function(){
+    buttonVideo.classList.remove('active');
+    videoProject.classList.remove('active');
+}
